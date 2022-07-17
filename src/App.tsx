11bc451168567a9df330ed7ece8b192430835c85
starunz <busouza01@gmail.com>
2022-07-17 01:24:44 -0300

    chore: altered primary color; corrected spacing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-
 import { ThemeProvider } from '@emotion/react';
 import { createTheme } from '@mui/material';
 
 import Home from './home';
 
-function App() {
+const App = () => {
 	const theme = createTheme({
 		palette: {
 			primary: {
-				main: '#388e3c',
+				main: '#000000',
 			},
 			secondary: {
 				main: '#1976d2',
@@ -19,10 +18,9 @@ function App() {
 
 	return (
 		<ThemeProvider theme={theme}>
-
 			<Home />
 		</ThemeProvider>
 	);
-}
+};
 
 export default App;
