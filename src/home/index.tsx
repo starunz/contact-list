a4b0f77686c1ad54d2b2aafe46828567819fa3e4
starunz <busouza01@gmail.com>
2022-07-17 01:27:46 -0300

    chore: Clean home; created contact list component

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,60 +1,9 @@
-import React from 'react';
-
 import {
-	Avatar,
-	Box,
 	Container,
-	List,
-	ListItem,
-	ListItemAvatar,
-	ListItemText,
 	Typography
 } from '@mui/material';
 
-const arrayContact = [
-	{
-		img: 'uee',
-		name: 'ue',
-		email: 'ue@ue.com',
-		phone: 99999999
-	},
-	{
-		img: 'uee',
-		name: 'ue',
-		email: 'ue@ue.com',
-		phone: 99999999
-	},
-	{
-		img: 'uee',
-		name: 'ue',
-		email: 'ue@ue.com',
-		phone: 99999999
-	},
-	{
-		img: 'uee',
-		name: 'ue',
-		email: 'ue@ue.com',
-		phone: 99999999
-	},
-	{
-		img: 'uee',
-		name: 'ue',
-		email: 'ue@ue.com',
-		phone: 99999999
-	},
-	{
-		img: 'uee',
-		name: 'ue',
-		email: 'ue@ue.com',
-		phone: 99999999
-	},
-	{
-		img: 'uee',
-		name: 'ue',
-		email: 'ue@ue.com',
-		phone: 99999999
-	}
-];
+import ContactList from '../components/contactList';
 
 const Home = () => {
 	return (
@@ -70,57 +19,7 @@ const Home = () => {
 				Contact List
 			</Typography>
 
-			{
-				arrayContact.map((contact) => (
-					<Box
-						sx={{
-							mt: 2,
-							mb: 2,
-							mx: 'auto',
-							boxShadow: '0 4px 4px rgba(0,0,0,0.5)',
-							maxWidth: 500,
-						}}
-					>
-
-						<List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-							<ListItem>
-
-								<ListItemAvatar>
-									<Avatar>
-										{contact.img}
-									</Avatar>
-								</ListItemAvatar>
-
-								<ListItemText
-									primary={contact.name}
-									secondary={
-										<React.Fragment>
-											<Typography
-												sx={{ display: 'block' }}
-												component="span"
-												variant="body2"
-												color="text.primary"
-											>
-												{contact.email}
-											</Typography>
-
-											<Typography
-												sx={{ display: 'block' }}
-												component="span"
-												variant="body2"
-												color="text.primary"
-											>
-												{contact.phone}
-											</Typography>
-										</React.Fragment>
-									}
-								/>
-							</ListItem>
-						</List>
-					</Box>
-
-				))
-			}
+			<ContactList />
 		</Container>
 	);
 };
