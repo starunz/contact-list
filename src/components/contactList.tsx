d1031da03964dab337a35419d8c512fa32bb0a72
starunz <busouza01@gmail.com>
2022-07-18 23:15:04 -0300

    feat: State changes and visual improvements

--- src/components/contactList.tsx
+++ src/components/contactList.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
 
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import SearchContact from './searchContact';
 import CreateContact from './createContact';
 import ContactItem from './contactItem';
 
 import CircularProgress from '@mui/material/CircularProgress';
-import Swal from 'sweetalert2';
 
 import * as api from '../services/api';
 
@@ -21,27 +20,68 @@ export interface Contact {
 
 const ContactList = () => {
 	const [contacts, setContacts] = useState<Contact[]>([]);
-	const [value, setValue] = React.useState<Contact | null>(null);
+	const [value, setValue] = useState<Contact | null>(null);
+	const [reloadObserver, setReloadObserver] = useState<object>({});
+	const [haveContacts, setHaveContacts] = useState<boolean>(true);
 
 	useEffect(() => {
 		const promise = api.getContacts();
 
 		promise.then(response => {
 			const contacts = response.data;
+			if (contacts.length === 0) setHaveContacts(false);
 			setContacts(contacts);
 		}).catch(error => {
-			Swal.fire({
-				icon: 'error',
-				title: 'OOPS...',
-				text: 'Ocorreu um erro, por favor tente novamente',
-			});
+			if(error.response.status === 409)
+				Swal.fire({
+					icon: 'error',
+					title: 'OOPS...',
+					text: 'Ocorreu um erro, por favor tente novamente!',
+				});
 		});
 
-	}, []);
+	}, [reloadObserver]);
+
+	const makeReloadContacts = () => {
+		setReloadObserver({});
+		setValue(null);
+		setHaveContacts(true);
+	};
+
+	if (!haveContacts) {
+		return (
+			<>
+				<CreateContact
+					contact={contacts}
+					makeReloadContacts={makeReloadContacts}
+				/>
+
+				<SearchContact
+					contacts={contacts}
+					value={value}
+					setValue={setValue}
+				/>
+			
+				<Box sx={{ maxWidth: 600, mx: 'auto', mt: 18 }} >
+					<Typography
+						sx={{ wordWrap: 'break-word', fontSize: 18, fontFamily: 'Roboto' }}
+						component="span"
+						variant="subtitle1"
+						color="text.primary"
+					>
+						Sem contatos ainda, experimente adicionar um novo!
+					</Typography>
+				</Box>
+			</>
+		);
+	}
 
 	return (
 		<>
-			<CreateContact />
+			<CreateContact
+				contact={contacts}
+				makeReloadContacts={makeReloadContacts}
+			/>
 
 			<SearchContact
 				contacts={contacts}
@@ -54,6 +94,7 @@ const ContactList = () => {
 					(value ? [value] : contacts).map((contact) => (
 						<ContactItem 
 							contact={contact}
+							makeReloadContacts={makeReloadContacts}
 						/>
 					))
 					:
